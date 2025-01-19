9710fcb6b164465374740b7119e1aab05ab0fa12
dgopadakak <dgopadakakv3@gmail.com>
2025-01-19 17:11:09 +0300

    Dealing with warnings during assembly

--- core/local_storage/src/main/java/com/dgopadakak/tagsgallery/core/local_storage/room/TagDao.kt
+++ core/local_storage/src/main/java/com/dgopadakak/tagsgallery/core/local_storage/room/TagDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.dgopadakak.tagsgallery.core.local_storage.room.models.Tag
 import com.dgopadakak.tagsgallery.core.local_storage.room.models.TagWithMedia
 import kotlinx.coroutines.flow.Flow
@@ -20,6 +21,7 @@ interface TagDao {
     @Delete
     suspend fun deleteTag(tag: Tag)
 
+    @Transaction
     @Query("SELECT * FROM Tag WHERE id = :tagId")
     fun getTagWithMedia(tagId: Long): Flow<TagWithMedia>
 }
