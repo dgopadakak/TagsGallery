f9a0b985ad01a18a276aed21998adade5c4846cb
dgopadakak <dgopadakakv3@gmail.com>
2025-02-15 13:46:34 +0300

    TAG-7 Interaction with the local storage via the Repository

--- core/local_storage/src/main/java/com/dgopadakak/tagsgallery/core/local_storage/Repository.kt
+++ core/local_storage/src/main/java/com/dgopadakak/tagsgallery/core/local_storage/Repository.kt
@@ -1,5 +1,28 @@
 package com.dgopadakak.tagsgallery.core.local_storage
 
-class Repository {
-    // TODO: реализовать взаимодействие через данный класс, а не напрямую
+import com.dgopadakak.tagsgallery.core.local_storage.models.MediaTagCrossRef
+import com.dgopadakak.tagsgallery.core.local_storage.models.Tag
+import com.dgopadakak.tagsgallery.core.local_storage.models.TagWithMedia
+import com.dgopadakak.tagsgallery.core.local_storage.room.TagDao
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * По сути это класс-обертка, он создан для того, чтобы не взаимодействовать напрямую с TagDao, что
+ * будет полезно при миграции проекта на KMP. Так как TagDao будет только для Android, а на других
+ * платформах данный класс будет работать с другими источниками данных.
+ */
+class Repository(private val tagDao: TagDao) {
+
+    fun getAllTags(): Flow<List<Tag>> = tagDao.getAllTags()
+
+    suspend fun insertTag(tag: Tag): Long = tagDao.insertTag(tag)
+
+    suspend fun updateTag(tag: Tag) = tagDao.updateTag(tag)
+
+    suspend fun deleteTag(tag: Tag) = tagDao.deleteTag(tag)
+
+    fun getTagWithMedia(tagId: Long): Flow<TagWithMedia?> = tagDao.getTagWithMedia(tagId)
+
+    suspend fun insertMediaTagCrossRef(crossRef: MediaTagCrossRef) =
+        tagDao.insertMediaTagCrossRef(crossRef)
 }
--- search/src/main/java/com/dgopadakak/tagsgallery/search/SearchViewModel.kt
+++ search/src/main/java/com/dgopadakak/tagsgallery/search/SearchViewModel.kt
@@ -2,7 +2,7 @@ package com.dgopadakak.tagsgallery.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dgopadakak.tagsgallery.core.local_storage.room.TagDao
+import com.dgopadakak.tagsgallery.core.local_storage.Repository
 import com.dgopadakak.tagsgallery.core.local_storage.models.Tag
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -13,17 +13,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val tagDao: TagDao
+    private val repository: Repository
 ) : ViewModel() {
 
     ////////////////////////////// Для теста! Удалить. //////////////////////////////
-    val tags: Flow<List<Tag>> = tagDao.getAllTags()
+    val tags: Flow<List<Tag>> = repository.getAllTags()
     private val _savedMediaUris = MutableStateFlow<List<String>?>(emptyList())
     val testSavedMediaUris = _savedMediaUris.asStateFlow()
 
     fun loadMediaForTag(tagId: Long) {
         viewModelScope.launch {
-            tagDao.getTagWithMedia(tagId).collect { tagWithMedia ->
+            repository.getTagWithMedia(tagId).collect { tagWithMedia ->
                 _savedMediaUris.value = tagWithMedia?.media?.map { it.mediaId }
             }
         }
